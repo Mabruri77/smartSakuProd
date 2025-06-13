307a957c93ad43c2fe950e90bff77821ee645365
Mabruri101 <muhammadmabruri99@gmail.com>
2025-06-13 02:25:07 -0400

    finish

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = Hapi.server({
-  port: 3000,
+  port: 80,
   host: "localhost",
 });
 await server.register(Inert);
